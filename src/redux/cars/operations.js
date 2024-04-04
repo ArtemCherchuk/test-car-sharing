e075bd0ac635e46d17993c6c6503574d5af95c56
ArtemCherchuk <250artem250@gmail.com>
2024-04-04 12:01:32 +0200

    upd

--- src/redux/cars/operations.js
+++ src/redux/cars/operations.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'https://65f1c1ac034bdbecc7639a65.mockapi.io/',
+  baseURL: 'http://65f1c1ac034bdbecc7639a65.mockapi.io/',
 });
 
 export const fetchCars = createAsyncThunk(
@@ -12,7 +12,6 @@ export const fetchCars = createAsyncThunk(
       const { data } = await instance.get(`/advert`, {
         params: { page: page, limit: 12 },
       });
-
       return data;
     } catch (error) {
       return thunkApi.rejectWithValue(error.message);
