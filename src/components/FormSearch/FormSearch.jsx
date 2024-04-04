412af9cd7010f300fa118ae5c702171bb0711dcb
ArtemCherchuk <250artem250@gmail.com>
2024-04-04 11:42:19 +0200

    add style form search

--- src/components/FormSearch/FormSearch.jsx
+++ src/components/FormSearch/FormSearch.jsx
@@ -9,34 +9,45 @@ export const FormSearch = ({ setIsFiltered }) => {
   const onHandleSubmit = e => {
     e.preventDefault();
     const value = e.currentTarget[0].value;
+    if (value === 'Enter the text') {
+      alert('Please select car brand');
+      return;
+    }
     dispatch(filterValue(value));
     setIsFiltered(true);
   };
 
   const onResetSearch = e => {
-    e.currentTarget.parentElement[0].value = 'Enter the text';
+    e.currentTarget.parentElement.parentElement[0].value = 'Enter the text';
     dispatch(filterValue(''));
     setIsFiltered(false);
   };
 
   return (
-    <form onSubmit={onHandleSubmit}>
-      <select name="makes">
-        <option value="Enter the text" hidden>
-          Enter the text
-        </option>
-        {makes.map(item => {
-          return (
-            <option value={item} key={item}>
-              {item}
-            </option>
-          );
-        })}
-      </select>
-      <button type="button" onClick={onResetSearch}>
-        Reset
-      </button>
-      <button type="submit">Search</button>
+    <form onSubmit={onHandleSubmit} className={css.form}>
+      <div className={css.selectContainer}>
+        <label className={css.label}>Car brand</label>
+        <select name="makes" className={css.select}>
+          <option value="Enter the text" hidden>
+            Enter the text
+          </option>
+          {makes.map(item => {
+            return (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div className={css.buttonContainer}>
+        <button type="button" onClick={onResetSearch} className={css.button}>
+          Reset
+        </button>
+        <button type="submit" className={css.button}>
+          Search
+        </button>
+      </div>
     </form>
   );
 };
